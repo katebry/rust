07e05d1afd673095477100959d545fc8f357d54e
katebry <katebryan31@gmail.com>
2023-02-26 19:33:48 +0000

    nested enums

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -1,13 +1,22 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arkansas
+}
+
 enum Coin {
     Penny, 
     Nickel,
     Dime, 
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
     let coin_val = value_in_cents(Coin::Penny);
+    let quarter_val = value_in_cents(Coin::Quarter(UsState::Arkansas));
     println!("{}", coin_val);
+    println!("{}", quarter_val);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -18,6 +27,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5, 
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
