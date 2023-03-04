55f25c30c9282b4fb7f4ddbfc5af00ffc40af23d
katebry <katebryan31@gmail.com>
2023-03-04 09:37:20 +0000

    Added mixup generic types

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,15 +1,26 @@
 use std::cmp::PartialOrd;
 
-struct Point<T, U> {
-    x: T,
-    y: U,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 fn main() {
 
-    let integer = Point {x: 5, y: 10};
-    let float = Point {x: 1.0, y: 4.0};
-    let integer_and_float = Point {x: 20, y: 3.4};
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 
     let number_list = vec![34, 50, 25, 100, 65];
 
