9c13ecd5dc1a50762e6b8721ac1dd7ff7d174663
katebry <katebryan31@gmail.com>
2023-03-05 20:34:36 +0000

    added more test cases

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -18,6 +18,10 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -64,5 +68,15 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Me");
+        assert!(
+            result.contains("Kate"),
+            "Greeting does not contain name, value was {}", 
+            result
+        );
     }
 }
