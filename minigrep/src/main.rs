c59c96c53dac3cd3131697e25f72ef9045040308
katebry <katebryan31@gmail.com>
2023-03-09 10:01:19 +0000

    Added run function and additional error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,11 +13,11 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    
-    let contents = fs::read_to_string(config.file_path)
-    .expect("Should have been able to read the file");
-
-    println!("With text: \n{contents}");
+  
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -37,4 +38,9 @@ impl Config {
     }
 }
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
+    println!("With text: \n{contents}");
+    Ok(())
+}
